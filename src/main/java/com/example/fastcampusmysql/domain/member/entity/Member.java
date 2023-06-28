5f9de554aad1948b7b63e03dc8f59475faeb123d
seungchan baek <poketsc@gmail.com>
2023-06-28 21:21:04 +0900

    회원이름 변경 + 테스트 코드 작성

--- src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
@@ -33,7 +33,14 @@ public class Member {
         this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
     }
 
-    void validateNickName(String nickName) {
+    public void changeNickname(String to) {
+        Objects.requireNonNull(to);
+        validateNickName(to);
+        nickname = to;
+    }
+
+    private void validateNickName(String nickName) {
         Assert.isTrue(nickName.length() <= NAME_MAX_LENGTH, "최대 길이를 초과했습니다.");
     }
+
 }
