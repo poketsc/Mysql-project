f17be40bf7efac428cead7b73a3e661216db8abb
seungchan baek <poketsc@gmail.com>
2023-06-29 13:12:38 +0900

    내가 쓴 글 업로드, 조회(날짜별) 기능 구현

---
+++ src/main/java/com/example/fastcampusmysql/domain/post/service/PostReadService.java
@@ -0,0 +1,20 @@
+package com.example.fastcampusmysql.domain.post.service;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class PostReadService {
+    final private PostRepository postRepository;
+
+    public List<DailyPostCount> getDailyPostCounts(DailyPostCountRequest request) {
+        return postRepository.groupByCreatedDate(request);
+    }
+
+}
